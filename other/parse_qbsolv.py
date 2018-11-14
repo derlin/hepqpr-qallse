d8d996582d393997b75a7affcb1a4d30b3136aec
Lucy Linder <lucy.derlin@gmail.com>
2018-11-14 09:19:58 -0800

    show solution type in markers (wip)

--- other/parse_qbsolv.py
+++ other/parse_qbsolv.py
@@ -1,6 +1,22 @@
 import click
 import re
 
+SOL_NEW_BEST = 'new best energy'
+SOL_NEW = 'new energy'
+SOL_DUPLICATE_BEST = 'best energy (dup)'
+SOL_DUPLICATE = 'duplicate energy'
+SOL_NOTHING = ''
+
+sol_types = dict(
+    NOTHING=0,  # nothing new, do nothing
+    NEW_HIGH_ENERGY_UNIQUE_SOL=1,  # solution is unique, highest new energy
+    DUPLICATE_HIGHEST_ENERGY=2,  # two cases, solution is unique, duplicate energy
+    DUPLICATE_ENERGY=3,  # two cases, solution is duplicate highest energy
+    DUPLICATE_ENERGY_UNIQUE_SOL=4,  # two cases, solution is unique, highest energy
+    NEW_ENERGY_UNIQUE_SOL=5  # solution is unique, new highest energy
+)
+colors = ['black', 'red', 'darkred', 'lightblue', 'blue', 'yellow']
+
 
 class Iterator:
 
@@ -22,6 +38,10 @@ class Iterator:
         return self.len
 
 
+_extract_time = lambda line: float(re.search('^\d+\.\d*', line)[0])
+_extract_energy = lambda line: float(re.match('.*((loop =)|(answer  ))(-?\d+\.\d*)', line).groups()[-1])
+
+
 def parse(lines):
     started = False
     lines = Iterator(lines)
@@ -29,6 +49,7 @@ def parse(lines):
     best_energy = None
     answers = []
     times = []
+    annotations = []
 
     while lines.has_next():
         line = lines.next()
@@ -38,22 +59,24 @@ def parse(lines):
                 started = True
                 while 'Starting outer loop' not in line:
                     line = lines.next()  # skip the state dump
-                times.append(float(re.search('^\d+\.\d*', line)[0]))
-                answers.append(float(re.match('.*loop =(-?\d+\.\d*)', line).group(1)))
+                times.append(_extract_time(line))
+                answers.append(_extract_energy(line))
+                annotations.append('NEW_HIGH_ENERGY_UNIQUE_SOL')
 
         elif 'after partition pass' in line:
             while not 'Latest answer' in line:
                 line = lines.next()
-            times.append(float(re.search('^\d+\.\d*', line)[0]))
-            answers.append(float(re.match('.*answer  (-?\d+\.\d*)', line).group(1)))
+            times.append(_extract_time(line))
+            answers.append(_extract_energy(line))
+            annotations.append(lines.next().strip().split(' ')[0])
             while not 'V Best outer loop' in line:
                 line = lines.next()
-            best_energy = float(re.match('.*loop =(-?\d+\.\d*)', line).group(1))
+            best_energy = _extract_energy(line)
 
-    return times, answers, best_energy
+    return times, answers, best_energy, annotations
 
 
-def plot_energies(times, answers):
+def plot_energies(times, answers, annotations):
     from plotly.offline import plot
     import plotly.graph_objs as go
     import numpy as np
@@ -64,7 +87,10 @@ def plot_energies(times, answers):
         x=times,
         y=answers,
         mode='lines+markers',
-        name='solutions over time'
+        name='solutions over time',
+        text=annotations,
+        hoverinfo='x+y+text',
+        marker=dict(color=[colors[sol_types[a]] for a in annotations])
     ), go.Scatter(
         x=[times[best_idx], times[-1]],
         y=[best] * 2,
@@ -79,13 +105,13 @@ def plot_energies(times, answers):
 @click.option('--plot/--no-plot', is_flag=True, default=True)
 def cli(input, plot):
     with open(input) as f:
-        times, answers, best_energy = parse(f.readlines())
+        times, answers, best_energy, annotations = parse(f.readlines())
         print(','.join(map(str, answers)))
         print(','.join(map(str, times)))
         print('Best: ', best_energy)
 
         if plot:
-            plot_energies(times, answers)
+            plot_energies(times, answers, annotations)
     print('done')
 
 
