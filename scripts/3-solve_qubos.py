e4d2509277d6c3f8f24a0ed0365811c10997b44f
Lucy Linder <lucy.derlin@gmail.com>
2019-01-21 17:02:46 -0800

    improve benchmark script example

--- scripts/3-solve_qubos.py
+++ scripts/3-solve_qubos.py
@@ -22,22 +22,82 @@ loglevel = logging.DEBUG
 
 events = [1000]
 dss = [10]
+repeat = 1
 
-data_path = '/tmp/hpt-collapse/ds{ds}/event00000{event}-hits.csv' # path to the datasets
+data_path = '/tmp/hpt-collapse/ds{ds}/event00000{event}-hits.csv'  # path to the datasets
 
-qubo_path = '/tmp' # path where the qubos are pickled
-qubo_prefix = 'evt{event}-ds{ds}-' # prefix for the qubo files
-output_path = '/tmp/' # where to serialize the responses
-output_prefix = qubo_prefix # prefix for serialized responses
+qubo_path = '/tmp'  # path where the qubos are pickled
+qubo_prefix = 'evt{event}-ds{ds}-'  # prefix for the qubo files
+output_path = '/tmp/'  # where to serialize the responses
+output_prefix = qubo_prefix  # prefix for serialized responses
 
-solver = 'neal' # solver to use
-solver_config = dict() # parameters for the solver. Note that "seed" is generated later.
+solver = 'neal'  # solver to use
+solver_config = dict()  # parameters for the solver. Note that "seed" is generated later.
 
 # ==== configure logging
 
 init_logging(logging.DEBUG, sys.stdout)
 
+
 # ==== build model
+
+def run_one(event, ds):
+    # load data
+    path = data_path.format(event=event, ds=ds)
+    dw = DataWrapper.from_path(path)
+    qubo_filepath = op.join(qubo_path, qubo_prefix.format(event=event, ds=ds) + 'qubo.pickle')
+
+    with open(qubo_filepath, 'rb') as f:
+        Q = pickle.load(f)
+    en0 = dw.compute_energy(Q)
+
+    for i in range(repeat):
+        # set seed
+        seed = random.randint(0, 1 << 30)
+        solver_config['seed'] = seed
+
+        # build model
+        with time_this() as time_info:
+            with time_this() as qtime_info:
+                if solver == 'neal':
+                    response = solve_neal(Q, **solver_config)
+                elif solver == 'qbsolv':
+                    response = solve_qbsolv(Q, **solver_config)
+                elif solver == 'dwave':
+                    response = solve_dwave(Q, **solver_config)
+                else:
+                    raise Exception('Invalid solver name.')
+
+            final_doublets, final_tracks = process_response(response)
+
+        # compute scores
+        p, r, ms = dw.compute_score(final_doublets)
+        trackml = dw.compute_trackml_score(final_tracks)
+        en = response.record.energy[0]
+
+        # output composition
+        _, _, d_real = diff_rows(dw.get_real_doublets(), final_doublets)
+        _, d_fakes, d_real_all = diff_rows(dw.get_real_doublets(with_unfocused=True), final_doublets)
+
+        # save response
+        output_filename = op.join(
+            output_path,
+            output_prefix.format(event=event, ds=ds) + f'{solver}-{i}-response.pickle')
+
+        with open(output_filename, 'wb') as f:
+            pickle.dump(response, f)
+
+        # gather stats
+        mat.append(
+            [
+                event, ds, seed, i,
+                len(final_tracks),
+                len(dw.get_real_doublets()), len(final_doublets),
+                len(d_real), len(d_real_all), len(d_fakes),
+                p, r, trackml, len(ms),
+                en, en0, en - en0,
+            ] + qtime_info + time_info)
+
 
 if __name__ == '__main__':
     mat = []
@@ -45,63 +105,13 @@ if __name__ == '__main__':
     for event in events:
         for ds in dss:
             print(f'\n==========>>> processing event {event} ds{ds}\n', flush=True)
+            run_one(event, ds)
 
-            # load data
-            path = data_path.format(event=event, ds=ds)
-            dw = DataWrapper.from_path(path)
-            qubo_filepath = op.join(qubo_path, qubo_prefix.format(event=event, ds=ds) + 'qubo.pickle')
-            with open(qubo_filepath, 'rb') as f:
-                Q = pickle.load(f)
+    headers = 'event,percent,seed,repeat,tracks_gen,' \
+              'n_true,n_gen,n_real,n_real_all,n_fakes,' \
+              'precision,recall,trackml,missings,' \
+              'en,en0,endiff,' \
+              'qtime_cpu,qtime_wall,cpu_time,wall_time'
 
-            # set seed
-            seed = random.randint(0, 1<<30)
-            solver_config['seed'] = seed
-
-            # build model
-            with time_this() as time_info:
-                with time_this() as qtime_info:
-                    if solver == 'neal':
-                        response = solve_neal(Q, **solver_config)
-                    elif solver == 'qbsolv':
-                        response = solve_qbsolv(Q, **solver_config)
-                    elif solver == 'dwave':
-                        response = solve_dwave(Q, **solver_config)
-                    else:
-                        raise Exception('Invalid solver name.')
-
-                final_doublets, final_tracks = process_response(response)
-
-            # compute scores
-            p, r, ms = dw.compute_score(final_doublets)
-            trackml = dw.compute_trackml_score(final_tracks)
-            en0 = dw.compute_energy(Q)
-            en = response.record.energy[0]
-
-            # output composition
-            _, _, d_real = diff_rows(dw.get_real_doublets(), final_doublets)
-            _, d_fakes, d_real_all = diff_rows(dw.get_real_doublets(with_unfocused=True), final_doublets)
-
-            # save response
-            output_filename = op.join(output_path, output_prefix.format(event=event, ds=ds) + f'{solver}-response.pickle')
-            with open(output_filename, 'wb') as f:
-                pickle.dump(response, f)
-
-            # gather stats
-            mat.append(
-                [
-                    event, ds, seed,
-                    len(final_tracks),
-                    len(dw.get_real_doublets()), len(final_doublets),
-                    len(d_real), len(d_real_all), len(d_fakes),
-                    p, r, trackml, len(ms),
-                    en, en0, en-en0,
-                ] + qtime_info + time_info)
-
-        headers = 'event,percent,seed,tracks_gen,' \
-                  'n_true,n_gen,n_real,n_real_all,n_fakes,' \
-                  'precision,recall,trackml,missings,' \
-                  'en,en0,endiff,' \
-                  'qtime_cpu,qtime_wall,cpu_time,wall_time'
-
-        stats = pd.DataFrame(mat, columns=headers.split(','))
-        stats.to_csv(f'solve_qubos_{solver}.csv', index=False)
+    stats = pd.DataFrame(mat, columns=headers.split(','))
+    stats.to_csv(f'solve_qubos_{solver}.csv', index=False)
